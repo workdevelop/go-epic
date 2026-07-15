af4dc2f185935fc0ef7c232640033abe89e6e14a
Maksym Pelypenko <max.softdev@i.ua>
2026-07-15 20:27:58 +0300

    feat: day 3

--- internal/models/character.go
+++ internal/models/character.go
@@ -5,3 +5,17 @@ type Character struct {
 	Health int
 	Position
 }
+
+func (o *Character) Move(dx, dy int) {
+	o.X += dx
+	o.Y += dy
+}
+
+func (o *Character) TakeDamage(amount int) int {
+	o.Health = max(o.Health - amount, 0)
+	return o.Health
+}
+
+func (o Character) IsAlive() bool {
+	return o.Health > 0
+}
